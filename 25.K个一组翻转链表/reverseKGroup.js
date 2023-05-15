85066d8510532ad38c87de0f94ced1718e445cb0
kite <2257440643@qq.com>
2023-05-15 20:33:58 +0800

    fix: author

--- 25.K个一组翻转链表/reverseKGroup.js
+++ 25.K个一组翻转链表/reverseKGroup.js
@@ -1,35 +1,44 @@
-const myReverse = (head, tail) => {
-  let prev = tail.next;
-  let p = head;
-  while (prev !== tail) {
-      const nex = p.next;
-      p.next = prev;
-      prev = p;
-      p = nex;
-  }
-  return [tail, head];
-}
-var reverseKGroup = function(head, k) {
-  const hair = new ListNode(0);
-  hair.next = head;
-  let pre = hair;
-
+/**
+ * @description: 栈方法     TC:O(n)  SC:O(n)
+ * @param {*} head  输入链表头部元素
+ * @param {*} k     翻转K个元素
+ * @return {*}
+ */
+function stack(head, k) {
+  /**
+   * 该方案利用栈先进后出的特点，每次遍历链表的k个元素入栈，然后将元素依次出栈添加到新的
+   * 链表中，直到遍历完成链表所有元素或剩余元素不足k个。剩余元素不足k个则直接将剩余元素
+   * 加入新链表中直接返回新链表
+   */
+  // 如果k小于等于1，证明无需翻转，直接返回原链表即可
+  if (k <= 1) return head;
+  // 定义新链表
+  const HEAD = new ListNode();
+  let node = HEAD;
+  // 遍历原链表
   while (head) {
-      let tail = pre;
-      // 查看剩余部分长度是否大于等于 k
-      for (let i = 0; i < k; ++i) {
-          tail = tail.next;
-          if (!tail) {
-              return hair.next;
+      // 利用数组模拟栈
+      let stackArray = [];
+      let cuNode = head
+      // 遍历K次，将前K个元素压入栈中
+      for (i = 0; i < k; i++) {
+          // 如果遍历到当前的元素为null，证明此次遍历剩余元素不足k个
+          // 将原链表剩余元素加入到新链表即可
+          if (!cuNode) {
+              node.next = head;
+              return HEAD.next;
           }
+          stackArray.push(cuNode);
+          cuNode = cuNode.next;
       }
-      const nex = tail.next;
-      [head, tail] = myReverse(head, tail);
-      // 把子链表重新接回原链表
-      pre.next = head;
-      tail.next = nex;
-      pre = tail;
-      head = tail.next;
+      // 退出栈中元素，将元素加入到新链表
+      for (let i = k - 1; i >= 0; i--) {
+          node.next = stackArray[i];
+          node = node.next;
+      }
   }
-  return hair.next;
-};
+  // 当链表元素刚好为k个元素，第一个元素变为最后一个
+  // 元素后其next指向的是第二个元素，将其重置为null
+  node.next = null;
+  return HEAD.next;
+}
